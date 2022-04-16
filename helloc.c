db67eafa2ee5b633c9f9e4f67d1a8530d77dc9a8
codefan <codefan@sina.com>
2022-04-16 20:33:55 +0800

    添加数学函数

--- helloc.c
+++ helloc.c
@@ -1,26 +1,10 @@
-
 #include <stdio.h>
 #include "./utils/file_util.h"
 
-void sortInt(int len, int * array){
-    //冒泡
-    for(int i=len-1;i>0; i--){
-        int sorted = 1;
-        for(int j=0; j<i; j++){
-            if(array[j] > array[j+1]){
-                sorted = 0;
-                int a = array[j+1];
-                array[j+1] = array[j];
-                array[j] = a;
-            }
-        }
-        if(sorted ==1){
-            break;
-        }
-    }
-}
-
 int main() {
+    fippolacArrary(8);
+    printf("\r\n");
+    /*
     int ** res = readIntArrayFromFile(".\\test\\input.txt");
     for(int i=1; i<=res[0][0]; i++){
         sortInt(res[0][1], res[i]);
@@ -29,4 +13,5 @@ int main() {
     writeIntArrayToFile(".\\test\\output.txt", res);
     releaseIntArray(res);
     return 0;
+    */
 }
---
+++ utils/math.h
@@ -0,0 +1,10 @@
+﻿#ifndef _MATH_UTIL_
+#define _MATH_UTIL_
+
+void sortInt(int len, int * array);
+/**
+ * int x 最大45
+ */
+int fippolacArrary(int x);
+
+#endif // _MATH_UTIL_
