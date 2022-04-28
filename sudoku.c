256bfc17149f37d7ed60e118c53aae540b578672
codefan <codefan@sina.com>
2022-04-28 16:37:57 +0800

    解决数独唯一解的问题，需要添加有多种可能解的解题思路

--- sudoku.c
+++ sudoku.c
@@ -5,6 +5,44 @@ int main() {
 
     int ** sudoku = readIntMatrixFromFile(".\\test\\sudoku.txt", 6, 6);
  
+    int fillNum = 6;
+    while(fillNum>0){
+        fillNum = 0;
+        for(int i=0; i<6; i++){
+            for(int j=0; j<6; j++){
+                if(sudoku[i][j]==0){
+                    int checkData[7] = {0,0,0,0,0,0,0};
+                    for(int k=0;k<6;k++){
+                        //检查行
+                        checkData[sudoku[i][k]]++;
+                        //检查列
+                        checkData[sudoku[k][j]]++;
+                    }
+                    //检查区域
+                    int  ar = i / 2;
+                    int  ac = j / 3;
+                    for(int c=0; c<3; c++){
+                        for(int r=0; r<2; r++){
+                            checkData[sudoku[ar*2+r][ac*3+c]]++;
+                        }
+                    }
+                    //找到唯一可以填写的数据
+                    int rc=0; int rv=0;
+                    for(int k=1;k<7;k++){
+                        if(checkData[k]==0){
+                            rc++;
+                            rv=k;
+                        }
+                    }
+                    if(rc==1){
+                        sudoku[i][j] = rv;
+                        fillNum ++;
+                    }
+                }            
+            }
+        }
+    }
+    
     printIntMatrix(sudoku, 6, 6);
 
     releaseIntMatrix(sudoku, 6);
