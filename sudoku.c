392b2bf6de88cb0f521a248a617a2a5b3ccc8303
codefan <codefan@sina.com>
2022-04-29 09:37:00 +0800

    添加宫内排除法，还没有调试

--- sudoku.c
+++ sudoku.c
@@ -1,19 +1,19 @@
 ﻿#include <stdio.h>
 #include "./utils/file_util.h"
 
-void sudokuSolution(int matrix, int row, int col)
+/**
+ * 填充所有可以确定的单元，一般对应初级难度的数独可以完成。
+ * @param sudoku 是一个二维数组，表示数独内容， 0 表示没有确定的数就是空
+ * @param matrix 数据的维度，比如6宫 9宫
+ * @param row 表示宫的行数
+ * @param col 表示宫的列数
+ */
+int fillAssuredItem(int **sudoku, int matrix, int row, int col)
 {
-    if (col * row != matrix || matrix > 16)
-    {
-        printf("输入的参数不正确！\n");
-        return;
-    }
-
-    int **sudoku = readIntMatrixFromFile(".\\test\\sudoku.txt", matrix, matrix);
-
-    int fillNum = 6;
+    int fillTotal = 0; // 总共填写的数字
     int hasError = 0;
-    while (fillNum > 0 && hasError == 0)
+    int fillNum; //记录本次循环找到的数字
+    do
     {
         fillNum = 0;
         for (int i = 0; i < matrix; i++)
@@ -65,34 +65,139 @@ void sudokuSolution(int matrix, int row, int col)
                 }
             }
         }
-    }
+        fillTotal = fillTotal + fillNum;
+    } while (fillNum > 0 && hasError == 0);
 
     if (hasError > 0)
     {
-        printf("出题错误，没有正确的解:\n");
+        return 0 - hasError;
     }
-    else
+    return fillTotal;
+}
+
+/**
+ * 检查数独是否已经全部填好，还有几个空格
+ * @param sudoku 是一个二维数组，表示数独内容， 0 表示没有确定的数就是空
+ * @param matrix 数据的维度，比如6宫 9宫
+ * @param row 表示宫的行数
+ * @param col 表示宫的列数
+ */
+int checkNotSureItems(int **sudoku, int matrix, int row, int col)
+{
+    int notSure = 0;
+    for (int i = 0; i < matrix; i++)
     {
-        int notSure = 0;
-        for (int i = 0; i < matrix; i++)
+        for (int j = 0; j < matrix; j++)
         {
-            for (int j = 0; j < matrix; j++)
+            if (sudoku[i][j] == 0)
             {
-                if (sudoku[i][j] == 0)
+                notSure++;
+            }
+        }
+    }
+    return notSure;
+}
+
+/**
+ * 检查 i,j 位置是否可以 填写 value 值
+ * @return 如果返回 0 表示冲突，1 表示可以
+ */
+int checkItemConflict(int **sudoku, int matrix, int row, int col, int i, int j, int value)
+{
+    for (int k = 0; k < matrix; k++)
+    {
+        //检查行
+        if (sudoku[i][k] == value || sudoku[k][j] == value)
+        {
+            return 0;
+        }
+    }
+    //检查区域
+    int ar = i / row;
+    int ac = j / col;
+    for (int r = 0; r < row; r++)
+    {
+        for (int c = 0; c < col; c++)
+        {
+            if (sudoku[ar * row + r][ac * col + c] == value)
+            {
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
+
+/**
+ * 用宫内排除法来填充数独
+ * @param sudoku 是一个二维数组，表示数独内容， 0 表示没有确定的数就是空
+ * @param matrix 数据的维度，比如6宫 9宫
+ * @param row 表示宫的行数
+ * @param col 表示宫的列数
+ */
+int inPalaceExclusion(int **sudoku, int matrix, int row, int col)
+{
+    int fillTotal = 0; // 总共填写的数字
+    int fillNum;       //记录每次循环填写的数字
+    do
+    {
+        fillNum = 0;
+        for (int i = 0; i < col; i++)
+        {
+            for (int j = 0; j < row; j++)
+            {
+                //找到功能还没有确定的数
+                int checkData[20] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+                for (int r = 0; r < row; r++)
                 {
-                    notSure++;
+                    for (int c = 0; c < col; c++)
+                    {
+                        checkData[sudoku[i * row + r][j * col + c]]++;
+                    }
+                }
+                //对每一个空缺的数字，在空格中是填，检查是否有冲突
+                for (int k = 1; k < matrix + 1; k++)
+                {
+                    int canFillSum = 0;
+                    int fillRow, fillCol;
+                    for (int r = 0; r < row; r++)
+                    {
+                        for (int c = 0; c < col; c++)
+                        {
+                            int canFill = checkItemConflict(sudoku, matrix, row, col,
+                                                            i * row + r, j * col + c, k);
+                            if (canFill == 1)
+                            {
+                                canFillSum++;
+                                fillRow = i * row + r;
+                                fillCol = j * col + c;
+                            }
+                        }
+                    }
+                    //找到唯一可以填写的地方
+                    if (canFillSum == 1)
+                    {
+                        sudoku[fillRow][fillCol] = k;
+                        fillNum++;
+                    }
                 }
             }
         }
-        if (notSure == 0)
-        {
-            printf("找到唯一解:\n");
-        }
-        else
-        {
-            printf("不是唯一解，有%d个待确定的宫格:\n", notSure);
-        }
+        fillTotal = fillTotal + fillNum;
+    } while (fillNum > 0); //如果一轮循环一个也没有找到，则推出循环
+    return fillTotal;
+}
+
+void sudokuSolution(char *filename, int matrix, int row, int col)
+{
+    if (col * row != matrix || matrix > 16)
+    {
+        printf("输入的参数不正确！\n");
+        return;
     }
+
+    int **sudoku = readIntMatrixFromFile(filename, matrix, matrix);
+
     printIntMatrix(sudoku, matrix, matrix);
 
     releaseIntMatrix(sudoku, matrix);
@@ -100,6 +205,6 @@ void sudokuSolution(int matrix, int row, int col)
 
 int main()
 {
-    sudokuSolution(9, 3, 3);
+    sudokuSolution(".\\test\\sudoku.txt", 9, 3, 3);
     return 0;
 }
