8dd3effb42bd7771080100ae77fb3f5380908aab
codefan <codefan@sina.com>
2022-08-18 14:18:24 +0800

    兼容mac

--- helloc.c
+++ helloc.c
@@ -2,12 +2,14 @@
 #include "./utils/math.h"
 #include "./utils/file_util.h"
 
-int main() {
-    //fielachian(8);
-    //printf("\r\n");
-    
-    int ** res = readIntArrayFromFile("./test/sudoku.txt");
-    for(int i=1; i<=res[0][0]; i++){
+int main()
+{
+    // fielachian(8);
+    // printf("\r\n");
+
+    int **res = readIntArrayFromFile("./test/input.txt");
+    for (int i = 1; i <= res[0][0]; i++)
+    {
         sortInt(res[0][1], res[i]);
     }
     printIntArray(res);
